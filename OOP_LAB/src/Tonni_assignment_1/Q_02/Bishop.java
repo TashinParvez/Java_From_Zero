371070ad84aee59903d4f1aced023193f0a921d4
Tashin Parvez <tashinparvez2002@gmail.com>
2022-12-30 17:54:55 +0600

    sababb vai assignment Q1 done

--- OOP_LAB/src/Tonni_assignment_1/Q_02/Bishop.java
+++
@@ -1,5 +0,0 @@
-package Tonni_assignment_1.Q_02;
-
-public class Bishop {
-
-}
---
+++ OOP_LAB/src/Tonni_assignment_1/Q_02/Main.java
@@ -0,0 +1,44 @@
+package Tonni_assignment_1.Q_02;
+
+final class Bishop{
+    public static int count=0;
+
+    Bishop() throws BishopCreationLimitExceeded {
+        count++;
+        abc();
+
+    }
+
+    public void printObjectNumber(){
+        System.out.println("This Bishop Object number is "+count+".");
+    }
+
+    public void abc() throws BishopCreationLimitExceeded{
+        try{
+            if(count>5){
+                throw new BishopCreationLimitExceeded();
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+}
+class BishopCreationLimitExceeded extends Exception{
+    BishopCreationLimitExceeded(){
+        super("The maximum number of bishop objects can be 5");
+    }
+
+}
+public class Main {
+    public static void main(String[] args) throws BishopCreationLimitExceeded {
+        Bishop b1=new Bishop();
+        Bishop b2=new Bishop();
+        Bishop b3=new Bishop();
+        Bishop b4=new Bishop();
+        Bishop b5=new Bishop();
+        Bishop b6=new Bishop();
+
+    }
+}
