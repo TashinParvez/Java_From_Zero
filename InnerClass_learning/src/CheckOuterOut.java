460c8c228f58fbf7e80aee92b4bebc0f0363e3a7
Tashin Parvez <tashinparvez2002@gmail.com>
2023-01-02 15:41:48 +0600

    innerClass

--- InnerClass_learning/src/CheckOuterOut.java
+++ InnerClass_learning/src/CheckOuterOut.java
@@ -1,29 +1,32 @@
 public class CheckOuterOut {
     public static void main(String[] args) {
-
-//        Outer obj = new Outer();
-//        obj.display();
+        Outer obj = new Outer();
+        obj.display(); // outer display
 
         Outer.Inner oi = new Outer().new Inner();
+        oi.y = 10;
+        oi.display(); // inner display
 
 
 
     }
 }
-class Outer{
+
+class Outer {
     static int x = 100;
-      class Inner{
-        int y =199;
-        void display()
-        {
+
+    class Inner {
+        int y = 199;
+
+        void display() {
             System.out.println("X+Y = " + (Outer.x + y));
         }
     }
-    void display()
-    {
+
+    void display() {
         Inner obj = new Inner();
         int z = obj.y;
-
+        System.out.println(z);
         obj.display();
     }
 }
---
+++ OOP_LAB/src/RodelaMaamCT4/src/CT4/lbltotal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CT4;
+
+/**
+ *
+ * @author User
+ */
+class lbltotal {
+
+    static void setText(String Grade) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
